1c0f1a23dab26e2b65c68a39538b5d5aba5f2794
ahmedosama <ahmedosamabedawy@gmail.com>
2024-12-24 22:31:51 +0200

    feat: :sparkles: Add Tailwind CSS configuration and import styles
    This commit adds the Tailwind CSS configuration file (tailwind.config.js) and imports the Tailwind CSS styles in the main styles.scss file.

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem !important',
+    },
+    opacity: {
+      "0-pct": "0%",
+      "5-pct": "5%",
+      "6-pct": "6%",
+      "10-pct": "10%",
+      "15-pct": "15%",
+      "16-pct": "16%",
+      "20-pct": "20%",
+      "25-pct": "25%",
+      "30-pct": "30%",
+      "35-pct": "35%",
+      "40-pct": "40%",
+      "45-pct": "45%",
+      "50-pct": "50%",
+      "55-pct": "55%",
+      "60-pct": "60%",
+      "65-pct": "65%",
+      "70-pct": "70%",
+      "75-pct": "75%",
+      "80-pct": "80%",
+      "85-pct": "85%",
+      "90-pct": "90%",
+      "95-pct": "95%",
+      "100-pct": "100%"
+    },
+    colors: {
+      ...colors,
+    },
+    extend: {
+      colors: {}
+    },
+  },
+  plugins: [
+    plugin(function({ addComponents  }) {
+      addComponents({
+      })
+    })
+  ],
+}
